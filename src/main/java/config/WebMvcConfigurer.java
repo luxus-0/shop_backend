4639daa314f27a353abae74335320ebaeb802ef7
Łukasz <luxus0@gmail.com>
2023-10-25 23:46:11 +0200

    feat: Add config module: Swagger and Mvc

--- src/main/java/config/WebMvcConfigurer.java
+++ src/main/java/config/WebMvcConfigurer.java
@@ -1,4 +1,4 @@
-package com.github.Shop.config;
+package config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
