ada6875b0884533aaf02fec29575c4c084ff6a63
Łukasz <luxus0@gmail.com>
2023-10-26 00:25:44 +0200

    Feature: add product module

---
+++ src/main/resources/db/01-create_product.sql
@@ -0,0 +1,8 @@
+CREATE TABLE PRODUCTS (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    price DECIMAL(19, 2) NOT NULL,
+    currency VARCHAR(3) NOT NULL,
+    description TEXT NOT NULL
+);
