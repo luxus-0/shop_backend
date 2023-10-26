b3de0bf238e0977a205d505fce945f066e6e47eb
Łukasz <luxus0@gmail.com>
2023-10-26 19:54:00 +0200

    Refactor : Removed unused validation

--- src/main/java/com/github/Shop/product/Product.java
+++ src/main/java/com/github/Shop/product/Product.java
@@ -2,9 +2,10 @@ package com.github.Shop.product;
 
 import com.github.Shop.currency.Currency;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
@@ -18,14 +19,9 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotEmpty
     private String name;
-    @NotEmpty
     private String category;
-    @Min(0)
     private BigDecimal price;
-    @NotEmpty
     private Currency currency;
-    @NotEmpty
     private String description;
 }
