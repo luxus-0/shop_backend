1d632ba45669001ba8c83707466d029f09e2a1c7
Łukasz <luxus0@gmail.com>
2023-10-28 19:11:37 +0200

    refactor: method upload image and serve files

--- src/main/java/com/github/Shop/image/ImageDataManager.java
+++ src/main/java/com/github/Shop/image/ImageDataManager.java
@@ -15,20 +15,18 @@ import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Objects;
 
 @Service
-@AllArgsConstructor
-@Log4j2
 public class ImageDataManager {
     @Value("${image.path}")
     private String imagePath;
 
     public ResponseUploadImage uploadImage(MultipartFile file) {
         String fileName = file.getOriginalFilename();
-        String changedName = UploadedFilesNameUtils.slugifyFileName(fileName);
+        String changedFileName = UploadedFilesNameUtils.slugifyFileName(fileName);
+        changedFileName = ExistingFileNameUtils.renameIfExists(Path.of(imagePath), changedFileName);
 
-        Path filePath = Paths.get(imagePath).resolve(changedName);
+        Path filePath = Paths.get(imagePath).resolve(changedFileName);
 
         try (InputStream inputStream = file.getInputStream()) {
             OutputStream outputStream = Files.newOutputStream(filePath);
@@ -39,8 +37,8 @@ public class ImageDataManager {
         return new ResponseUploadImage(fileName);
     }
 
-    Resource serveFiles(String fileName){
+    Resource serveFiles(String filename){
         FileSystemResourceLoader fileSystemResourceLoader = new FileSystemResourceLoader();
-       return fileSystemResourceLoader.getResource(imagePath + fileName);
+       return fileSystemResourceLoader.getResource(imagePath + filename);
     }
 }
