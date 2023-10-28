b0fcaccf7fcedf21aed02662f3c28d62c0164668
Łukasz <luxus0@gmail.com>
2023-10-28 19:09:23 +0200

    Refactor: product module

--- src/main/java/com/github/Shop/product/ProductRepository.java
+++ src/main/java/com/github/Shop/product/ProductRepository.java
@@ -1,10 +1,14 @@
 package com.github.Shop.product;
 
+import com.github.Shop.product.dto.ProductDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query("select p from Product p where p.slug = :slug")
+    ProductDto findProductBySlug(String slug);
 }
