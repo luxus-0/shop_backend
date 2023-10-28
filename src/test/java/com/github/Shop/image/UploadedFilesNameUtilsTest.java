a0b1a5bfaba9160715f013605e160bab7c247733
Łukasz <luxus0@gmail.com>
2023-10-28 15:16:09 +0200

    Testing: add slugify test method

---
+++ src/test/java/com/github/Shop/image/UploadedFilesNameUtilsTest.java
@@ -0,0 +1,23 @@
+package com.github.Shop.image;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.Assert.assertEquals;
+
+class UploadedFilesNameUtilsTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "test test.png, test-test.png",
+            "hello Lukasz.png, hello-lukasz.png",
+            "ąęśćżźńłó.png, aesczznlo.png",
+            "Product 1.png, product-1.png"
+    })
+    void shouldSlugifyFileName(String actualFileName, String expectedFileName){
+        String slugify = UploadedFilesNameUtils.slugifyFileName(actualFileName);
+
+        assertEquals(slugify, expectedFileName);
+    }
+
+}
