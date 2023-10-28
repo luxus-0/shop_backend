7fed7339a22b61d8a34e78097c5d25c80512253a
Łukasz <luxus0@gmail.com>
2023-10-28 11:18:45 +0200

    Feat: Add entity class uploaded image

--- src/main/java/com/github/Shop/image/dto/ResponseUploadImage.java
+++ src/main/java/com/github/Shop/image/dto/ResponseUploadImage.java
@@ -1,7 +1,18 @@
 package com.github.Shop.image.dto;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Builder;
 
 @Builder
-public record ResponseUploadImage(String path, String name, String type, Long size, Long lastModified) {
+@Entity
+public record ResponseUploadImage(@Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id,
+                                  String path,
+                                  String absolutePath,
+                                  String name,
+                                  String type,
+                                  Long size,
+                                  Long lastModified) {
 }
