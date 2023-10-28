85255545c61de49e6a2d9a2a2e16f5c80509bc0f
Łukasz <luxus0@gmail.com>
2023-10-28 22:32:15 +0200

    Create table Image

---
+++ src/main/resources/db/4.create_image.sql
@@ -0,0 +1,12 @@
+--liquibase formatted sql
+--changeset lnowogorski:4
+CREATE TABLE Image (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255),
+    type VARCHAR(255),
+    path VARCHAR(255)
+    product_id INT,
+    admin_product_id INT,
+    FOREIGN KEY (product_id) REFERENCES Product(id),
+    FOREIGN KEY(admin_product_id) REFERENCES AdminProduct(id)
+);
