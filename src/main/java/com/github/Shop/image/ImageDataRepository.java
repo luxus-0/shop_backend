1d0e109e2817ba8bf45b975b105cd8287c47d5ba
Łukasz <luxus0@gmail.com>
2023-10-28 15:13:51 +0200

    Feat: Add image module

--- src/main/java/com/github/Shop/image/ImageDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.Shop.image;
-
-import com.github.Shop.image.dto.ResponseUploadImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ImageDataRepository extends JpaRepository<ResponseUploadImage, UUID> {
-}
