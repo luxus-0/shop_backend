bfd293ddfe44242b2b68aa8374f6e3b2f0730f00
Łukasz <luxus0@gmail.com>
2023-11-01 13:39:09 +0100

    Change

--- src/main/java/com/github/Shop/constant/Constants.java
+++ src/main/java/com/github/Shop/constant/Constants.java
@@ -1,5 +1,5 @@
 package com.github.Shop.constant;
 
-public interface Constants {
-    String REGEX_SLUG = "[a-z0-9\\-]+";
+public class Constants {
+    public static final String REGEX_SLUG = "^[a-z0-9\\-]+";
 }
--- src/main/java/com/github/Shop/review/dto/ReviewDto.java
+++ src/main/java/com/github/Shop/review/dto/ReviewDto.java
@@ -1,9 +1,7 @@
 package com.github.Shop.review.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 
 @Getter
