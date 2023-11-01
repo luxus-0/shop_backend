cd5d9d9bfd7275933e1bbcbc862cf48d91adfd01
Łukasz <luxus0@gmail.com>
2023-11-01 19:40:52 +0100

    Add sql cart

--- src/main/resources/db/10.create-cart.sql
+++ src/main/resources/db/10.create-cart.sql
@@ -9,7 +9,9 @@ create table cartItem(
     id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY not null,
     product_id bigint not null,
     quantity int,
-    cart_id bigint not null,
+    cart_id bigint not null ,
     constraint fk_cart_item_product_id foreign key (product_id) references product(id),
     constraint fk_cart_item_cart_id foreign key (cart_id) references cart(id)
 );
+
+ALTER TABLE cartItem ALTER COLUMN cart_id SET NOT NULL;
