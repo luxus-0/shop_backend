92541ed177e8f8f0fbc21a3905b6bab817a739c2
Łukasz <luxus0@gmail.com>
2023-11-02 19:29:57 +0100

    add sql file

--- src/main/resources/db/01-create_product.sql
+++ src/main/resources/db/01-create_product.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 --changeset lnowogorski:1
 CREATE TABLE product (
-    id bigint PRIMARY KEY generated always as identity not null ,
+    id bigint PRIMARY KEY auto_increment not null ,
     name VARCHAR(255) NOT NULL,
     category VARCHAR(255) NOT NULL,
     price DECIMAL(19, 2) NOT NULL,
