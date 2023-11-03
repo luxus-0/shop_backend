38be918a005d77f57a0bee2f2474d9f47396ad62
Łukasz <luxus0@gmail.com>
2023-11-03 23:15:15 +0100

    create table address sql

--- src/main/resources/db/15.create-table-address.sql
+++ src/main/resources/db/15.create-table-address.sql
@@ -1,10 +1,11 @@
 -- liquibase formatted sql
--- changeset lnowogorski:14
+-- changeset lnowogorski:15
 create table address
 (
+    id serial primary key not null,
     street  varchar(255) not null,
     zipCode varchar(255) not null,
     city    varchar(255) not null,
-    customer_id varchar(255) not null,
+    customer_id bigint not null,
     constraint fk_customer_address foreign key (customer_id) REFERENCES  "Customer"(id)
 );
