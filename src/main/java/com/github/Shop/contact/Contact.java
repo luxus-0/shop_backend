2534b4959c94d3c89392b335f53c3416a87512e3
Łukasz <luxus0@gmail.com>
2023-11-03 23:16:57 +0100

    Add contact entity

--- src/main/java/com/github/Shop/contact/Contact.java
+++ src/main/java/com/github/Shop/contact/Contact.java
@@ -1,6 +1,9 @@
 package com.github.Shop.contact;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,6 +15,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String phone;
     private String email;
 }
