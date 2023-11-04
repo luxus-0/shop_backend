26a3ae8d8b2f4d5ed4e8ceea01d95fc7ba7e6f7d
Łukasz <luxus0@gmail.com>
2023-11-04 08:49:36 +0100

    refactor sql file

--- src/main/resources/db/12.create-table-order.sql
+++ src/main/resources/db/12.create-table-order.sql
@@ -8,11 +8,13 @@ CREATE TABLE "order" (
 );
 
 create table order_row(
-                          id bigint generated always as identity primary key not null,
-                          order_id bigint not null,
-                          product_id bigint not null,
+                          id bigint generated always as identity primary key,
+                          order_id bigint,
+                          product_id bigint,
+                          shipment_id bigint,
                           quantity int not null,
                           price decimal(6,2) not null,
                           constraint fk_order_row_order_id foreign key (order_id) references "order"(id),
-                          constraint fk_order_row_product_id foreign key (product_id) references product(id)
+                          constraint fk_order_row_product_id foreign key (product_id) references product(id),
+                          constraint fk_order_row_shipment_id foreign key (shipment_id) references shipment(id)
 );
