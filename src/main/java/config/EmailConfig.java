7e9e2841c792c2c6497f71a228727c279b1d8a93
Łukasz <luxus0@gmail.com>
2023-11-05 13:46:12 +0100

    Add email config

---
+++ src/main/java/config/EmailConfig.java
@@ -0,0 +1,16 @@
+package config;
+
+import com.github.Shop.mail.EmailSender;
+import com.github.Shop.mail.EmailService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+
+public class EmailConfig {
+    @Bean
+    @ConditionalOnProperty(name = "email.sender", havingValue = "emailService")
+    public EmailSender emailService(JavaMailSender javaMailSender){
+        return new EmailService(javaMailSender);
+
+    }
+}
