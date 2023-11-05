64f1ce06f2cb3e151c85417a8f9208a55b30cddf
Łukasz <luxus0@gmail.com>
2023-11-05 13:54:28 +0100

    Add @EnableAsync for async method

--- src/main/java/com/github/Shop/ShopApplication.java
+++ src/main/java/com/github/Shop/ShopApplication.java
@@ -2,10 +2,12 @@ package com.github.Shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync(proxyTargetClass = true)
 public class ShopApplication {
 
 	public static void main(String[] args) {
