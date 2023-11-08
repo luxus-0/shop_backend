482fef3fe955398d127498f53c89d88acadbeb6a
Łukasz <luxus0@gmail.com>
2023-11-08 19:58:58 +0100

    Add method find place date between start and end date with order status

--- src/main/java/com/github/Shop/adminorder/AdminOrderRepository.java
+++ src/main/java/com/github/Shop/adminorder/AdminOrderRepository.java
@@ -1,6 +1,11 @@
 package com.github.Shop.adminorder;
 
+import com.github.Shop.order.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface AdminOrderRepository extends JpaRepository<AdminOrder, Long> {
+    List<AdminOrder> findAllByPlaceDateBetweenAndOrderStatus(LocalDateTime start, LocalDateTime end, OrderStatus orderStatus);
 }
