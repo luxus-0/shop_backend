729452547564cb45177cc7ed6d8326896d2838bd
Łukasz <luxus0@gmail.com>
2023-11-08 19:59:40 +0100

    Add Constants Headers from Orders CSV FILE

--- src/main/java/com/github/Shop/constant/Constants.java
+++ src/main/java/com/github/Shop/constant/Constants.java
@@ -2,4 +2,5 @@ package com.github.Shop.constant;
 
 public class Constants {
     public static final String REGEX_SLUG = "^[a-z0-9\\-]+";
+    public static final String[] HEADERS = {"Id", "firstname", "lastname", "pesel", "city", "street", "zipcode", "email", "phone", "Place Date", "Order Status", "Gross Value", "Payment"};
 }
