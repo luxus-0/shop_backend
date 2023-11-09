f1a1211f603cab62e59badf242e25daadf4ff5b0
Łukasz <luxus0@gmail.com>
2023-11-09 20:37:31 +0100

    Add: Add exception statistics sales not found

---
+++ src/main/java/com/github/shop/adminorder/StatisticsSalesNotFoundException.java
@@ -0,0 +1,7 @@
+package com.github.shop.adminorder;
+
+public class StatisticsSalesNotFoundException extends Exception{
+    public StatisticsSalesNotFoundException(){
+        super("Statistics sales not found");
+    }
+}
