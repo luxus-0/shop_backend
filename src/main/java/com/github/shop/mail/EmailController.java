d5809dbfc3946d19bb0987b1be45e4d9eccefee3
Łukasz <luxus0@gmail.com>
2023-11-09 19:59:51 +0100

    Fix: Repair email controller.Change RequestParam to RequestBody Valid EmailDto

--- src/main/java/com/github/shop/mail/EmailController.java
+++ src/main/java/com/github/shop/mail/EmailController.java
@@ -1,14 +1,13 @@
 package com.github.shop.mail;
 
+import com.github.shop.mail.dto.EmailDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * SEND EMAIL TO CLIENT WITH BODY HTML
- */
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/emails")
@@ -17,7 +16,7 @@ public class EmailController {
     private final EmailService emailService;
 
     @PostMapping("/send")
-    public void sendEmail(@RequestParam("email") String to, @RequestParam("subject") String subject, @RequestParam("body") String body) {
-        emailService.send(to, subject, body);
+    public void sendEmail(@RequestBody @Valid EmailDto email) {
+        emailService.send(email);
     }
 }
