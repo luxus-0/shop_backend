0cbd99cfd003259dd81358c40d27d49b31e1cea3
Łukasz <luxus0@gmail.com>
2023-11-09 20:03:25 +0100

    Add: Added EmailDto class with field(email, subject, body) validation

--- src/main/java/com/github/shop/mail/EmailSender.java
+++ src/main/java/com/github/shop/mail/EmailSender.java
@@ -1,7 +1,8 @@
 package com.github.shop.mail;
 
+import com.github.shop.mail.dto.EmailDto;
 import jakarta.mail.MessagingException;
 
 public interface EmailSender {
-    void send(String to, String subject, String body) throws MessagingException;
+    void send(EmailDto email) throws MessagingException;
 }
