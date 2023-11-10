23843c0ec9084b77e98958cae9304ae37bb7e3e1
Łukasz <luxus0@gmail.com>
2023-11-10 17:38:39 +0100

    Add: Add Phone regex to class Contact

--- src/main/java/com/github/shop/constant/Constants.java
+++ src/main/java/com/github/shop/constant/Constants.java
@@ -2,5 +2,6 @@ package com.github.shop.constant;
 
 public class Constants {
     public static final String REGEX_SLUG = "^[a-z0-9\\-]+";
+    public static final  String PHONE_REGEX = "\\+48 \\d{3} \\d{3} \\d{3}";
     public static final String[] HEADERS = {"Id", "firstname", "lastname", "pesel", "city", "street", "zipcode", "email", "phone", "Place Date", "Order Status", "Gross Value", "Payment"};
 }
