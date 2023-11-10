bbd84c4bcc012d4cb9decb411bb5b4eab1583c0d
Łukasz <luxus0@gmail.com>
2023-11-10 17:38:58 +0100

    Add: Add Phone regex to class Contact

--- src/main/java/com/github/shop/contact/Contact.java
+++ src/main/java/com/github/shop/contact/Contact.java
@@ -5,11 +5,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import static com.github.shop.constant.Constants.PHONE_REGEX;
 
 @Entity
 @Getter
@@ -20,9 +23,10 @@ public class Contact {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotBlank
+    @NotNull
+    @Pattern(regexp = PHONE_REGEX)
     private String phone;
-    @NotBlank
+    @NotNull
     @Email
     private String email;
 }
