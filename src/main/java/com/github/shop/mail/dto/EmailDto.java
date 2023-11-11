0a9d546dea6a046a3e33259fa209b8ed8a2a2ddf
Łukasz <luxus0@gmail.com>
2023-11-11 16:01:04 +0100

    Add builder to class Email dto

--- src/main/java/com/github/shop/mail/dto/EmailDto.java
+++ src/main/java/com/github/shop/mail/dto/EmailDto.java
@@ -2,7 +2,9 @@ package com.github.shop.mail.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 
 
+@Builder
 public record EmailDto(@Email String email, @NotNull String subject,@NotNull String body) {
 }
