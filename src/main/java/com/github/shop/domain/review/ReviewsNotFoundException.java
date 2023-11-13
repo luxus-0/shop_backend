dcf4254e5890acf448128e9f76a2865795cdd22a
Łukasz <luxus0@gmail.com>
2023-11-13 23:03:26 +0100

    refactor: Remove unused class

--- src/main/java/com/github/shop/domain/review/ReviewsNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.github.shop.domain.review;
-
-public class ReviewsNotFoundException extends Exception{
-    public ReviewsNotFoundException(){
-        super("Review not found");
-    }
-}
