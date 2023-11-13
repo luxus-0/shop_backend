71dc18e7583fb4ffc2aae7b5e62ebf8ae0d64d66
Łukasz <luxus0@gmail.com>
2023-11-13 23:10:55 +0100

    Add : Add Login Controller

--- src/main/java/com/github/shop/infrastructure/security/login/LoginController.java
+++ src/main/java/com/github/shop/infrastructure/security/login/LoginController.java
@@ -1,14 +1,22 @@
 package com.github.shop.infrastructure.security.login;
 
+import com.github.shop.infrastructure.security.login.dto.UserDto;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.http.HttpStatus.CREATED;
+
 @RestController
+@AllArgsConstructor
 public class LoginController {
 
+    private final LoginFacade loginFacade;
     @PostMapping("/login")
-    public String login(@RequestBody Login login){
-        return null;
+    public ResponseEntity<UserDto> login(@RequestBody Login login) {
+        UserDto user = loginFacade.findByUsername(login.username());
+        return ResponseEntity.status(CREATED).body(user);
     }
 }
