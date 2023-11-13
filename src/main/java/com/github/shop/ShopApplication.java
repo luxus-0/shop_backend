0777933c59a53ebb2b4c5997946bc90aaa4d6d5f
Łukasz <luxus0@gmail.com>
2023-11-13 23:04:37 +0100

    Add: Add @EnableConfigurationProperties(value=JwtConfigurationProperties.class)

--- src/main/java/com/github/shop/ShopApplication.java
+++ src/main/java/com/github/shop/ShopApplication.java
@@ -1,13 +1,16 @@
 package com.github.shop;
 
+import com.github.shop.infrastructure.security.token.JwtConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync(proxyTargetClass = true)
+@EnableConfigurationProperties(value = JwtConfigurationProperties.class)
 public class ShopApplication {
 
     public static void main(String[] args) {
