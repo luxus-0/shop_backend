0ac0e61e108bdb6b925f7e87abdf184c92d2db66
Łukasz <luxus0@gmail.com>
2023-11-13 23:12:22 +0100

    Add : Add USER_NOT_FOUND = "user not found" for message security

--- src/main/java/com/github/shop/domain/constant/Constants.java
+++ src/main/java/com/github/shop/domain/constant/Constants.java
@@ -1,7 +1,8 @@
-package com.github.shop.constant;
+package com.github.shop.domain.constant;
 
 public class Constants {
     public static final String REGEX_SLUG = "^[a-z0-9\\-]+";
     public static final  String PHONE_REGEX = "\\+48 \\d{3} \\d{3} \\d{3}";
     public static final String[] HEADERS = {"Id", "firstname", "lastname", "pesel", "city", "street", "zipcode", "email", "phone", "Place Date", "Order Status", "Gross Value", "Payment"};
+    public static final String USER_NOT_FOUND = "user not found";
 }
