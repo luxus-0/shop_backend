b4761f2ec0a3a26ac9b7c928745f22f38ba66314
Łukasz <luxus0@gmail.com>
2023-11-14 23:59:18 +0100

    Add: add security module with validation

--- src/main/java/com/github/shop/ShopApplication.java
+++ src/main/java/com/github/shop/ShopApplication.java
@@ -1,5 +1,6 @@
 package com.github.shop;
 
+import com.github.shop.infrastructure.security.register.dto.RegisterUserDto;
 import com.github.shop.infrastructure.security.token.JwtConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,7 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync(proxyTargetClass = true)
-@EnableConfigurationProperties(value = JwtConfigurationProperties.class)
+@EnableConfigurationProperties(value = {JwtConfigurationProperties.class})
 public class ShopApplication {
 
     public static void main(String[] args) {
