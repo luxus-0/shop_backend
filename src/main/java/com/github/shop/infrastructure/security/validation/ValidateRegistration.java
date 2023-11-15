3ba3cfb2225d7aa1fb7ec044c95a56e4b198714f
Łukasz <luxus0@gmail.com>
2023-11-15 20:30:42 +0100

    Add validator registration: password validator - password == repeat password username validator - username exists in databases

---
+++ src/main/java/com/github/shop/infrastructure/security/validation/ValidateRegistration.java
@@ -0,0 +1,32 @@
+package com.github.shop.infrastructure.security.validation;
+
+import com.github.shop.infrastructure.security.login.LoginRepository;
+import com.github.shop.infrastructure.security.register.UserAlreadyExistsException;
+import com.github.shop.infrastructure.security.register.dto.RegisterUserDto;
+import com.github.shop.infrastructure.security.register.exception.PasswordNotTheSameException;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ValidateRegistration {
+
+    private final LoginRepository loginRepository;
+
+    public void validateRegistration(RegisterUserDto registerUser) throws PasswordNotTheSameException, UserAlreadyExistsException {
+        validatePassword(registerUser);
+        validateUsernameAvailability(registerUser.username());
+    }
+
+    private void validatePassword(RegisterUserDto registerUser) throws PasswordNotTheSameException {
+        if (!registerUser.password().equals(registerUser.repeatPassword())) {
+            throw new PasswordNotTheSameException();
+        }
+    }
+
+    private void validateUsernameAvailability(String username) throws UserAlreadyExistsException {
+        if (loginRepository.existsUserByUsername(username)) {
+            throw new UserAlreadyExistsException(username);
+        }
+    }
+}
