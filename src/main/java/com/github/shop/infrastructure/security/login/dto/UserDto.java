b407f5ee1005c6577078867a8b7f6b6bc38148b2
Łukasz <luxus0@gmail.com>
2023-11-15 20:29:03 +0100

    refactor import

--- src/main/java/com/github/shop/infrastructure/security/login/dto/UserDto.java
+++ src/main/java/com/github/shop/infrastructure/security/login/dto/UserDto.java
@@ -4,7 +4,6 @@ import com.github.shop.infrastructure.security.login.UserRole;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 
