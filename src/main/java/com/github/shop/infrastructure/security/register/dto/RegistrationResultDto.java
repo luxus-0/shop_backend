2f7aa8db26a75cd93e55df206a0e85b4d1af63fa
Łukasz <luxus0@gmail.com>
2023-11-15 20:57:23 +0100

    Add field String token

--- src/main/java/com/github/shop/infrastructure/security/register/dto/RegistrationResultDto.java
+++ src/main/java/com/github/shop/infrastructure/security/register/dto/RegistrationResultDto.java
@@ -2,8 +2,11 @@ package com.github.shop.infrastructure.security.register.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 
+@Builder
 public record RegistrationResultDto(Long id, boolean created,
                                     @Email @NotBlank(message = "${not.blank.username}")
-                                    String username) {
+                                    String username,
+                                    String token) {
 }
