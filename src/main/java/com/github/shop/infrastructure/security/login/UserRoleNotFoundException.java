8af94a170a3b4f85f9106e0c4b35243c8848a90e
Łukasz <luxus0@gmail.com>
2023-11-15 21:54:49 +0100

    Add unchecked exception - user role not found - with message

---
+++ src/main/java/com/github/shop/infrastructure/security/login/UserRoleNotFoundException.java
@@ -0,0 +1,9 @@
+package com.github.shop.infrastructure.security.login;
+
+import static com.github.shop.domain.constant.Constants.USER_ROLE_NOT_FOUND;
+
+public class UserRoleNotFoundException extends RuntimeException{
+    public UserRoleNotFoundException(){
+        super(USER_ROLE_NOT_FOUND);
+    }
+}
