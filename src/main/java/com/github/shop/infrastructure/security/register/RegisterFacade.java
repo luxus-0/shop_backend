d35b981247c523d9f29a0ab0e208848917e4dd21
Łukasz <luxus0@gmail.com>
2023-11-15 20:21:05 +0100

    Refactor method RegistrationResultDto register(RegisterUserDto registerUser)
    Add validator registration:
    existing username,
    Equals password(password equal repeat password)

--- src/main/java/com/github/shop/infrastructure/security/register/RegisterFacade.java
+++ src/main/java/com/github/shop/infrastructure/security/register/RegisterFacade.java
@@ -5,7 +5,10 @@ import com.github.shop.infrastructure.security.login.User;
 import com.github.shop.infrastructure.security.login.UserRole;
 import com.github.shop.infrastructure.security.register.dto.RegisterUserDto;
 import com.github.shop.infrastructure.security.register.dto.RegistrationResultDto;
+import com.github.shop.infrastructure.security.register.exception.PasswordNotTheSameException;
+import com.github.shop.infrastructure.security.validation.ValidateRegistration;
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -13,16 +16,21 @@ import java.util.List;
 
 @Component
 @AllArgsConstructor
+@Log4j2
 public class RegisterFacade {
     private final LoginRepository repository;
     private final PasswordEncoder bCryptPasswordEncoder;
-    public RegistrationResultDto register(RegisterUserDto registerUser){
+    private final ValidateRegistration validator;
+
+    public RegistrationResultDto register(RegisterUserDto registerUser) throws PasswordNotTheSameException, UserAlreadyExistsException {
+       validator.validateRegistration(registerUser);
         final User user = User.builder()
                 .username(registerUser.username())
                 .password(bCryptPasswordEncoder.encode(registerUser.password()))
+                .created(true)
                 .authorities(List.of(UserRole.ROLE_CUSTOMER))
                 .build();
         User savedUser = repository.save(user);
-        return new RegistrationResultDto(savedUser.getId(), true, savedUser.getUsername());
+        return new RegistrationResultDto(savedUser.getId(), savedUser.isCreated(), savedUser.getUsername());
     }
 }
