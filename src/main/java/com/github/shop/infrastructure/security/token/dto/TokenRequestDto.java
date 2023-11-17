e65b6ca3bebe9c7b8eb3948918312ce67b9b53f0
Łukasz <luxus0@gmail.com>
2023-11-17 20:50:37 +0100

    Add field Boolean created to record TokenRequestDto

--- src/main/java/com/github/shop/infrastructure/security/token/dto/TokenRequestDto.java
+++ src/main/java/com/github/shop/infrastructure/security/token/dto/TokenRequestDto.java
@@ -5,5 +5,6 @@ import jakarta.validation.constraints.NotBlank;
 public record TokenRequestDto(@NotBlank(message = "{username.not.blank}")
                               String username,
                               @NotBlank(message = "{password.not.blank}")
-                              String password) {
+                              String password,
+                              Boolean created) {
 }
