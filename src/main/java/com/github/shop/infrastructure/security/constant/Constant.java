db689d8a31f8d9698bd61e89955bfa894c1cdc86
Łukasz <luxus0@gmail.com>
2023-11-19 19:15:31 +0100

    Add Constant Hash link for lost password: http://localhost:4200/lostPassword

--- src/main/java/com/github/shop/infrastructure/security/constant/Constant.java
+++ src/main/java/com/github/shop/infrastructure/security/constant/Constant.java
@@ -2,4 +2,5 @@ package com.github.shop.infrastructure.security.constant;
 
 public class Constant {
     public static final String REGEX_PASSWORD = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!#@$%^&*-?)(!]).{8,}$";
+    public static final String HASH_LINK = "http://localhost:4200/lostPassword";
 }
